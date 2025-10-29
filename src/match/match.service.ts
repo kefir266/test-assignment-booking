d0fe7f28c1ff7e68218e4ebce827fd1b72b37394
Dmytro Horbatko <kefir266@yahoo.com>
2025-10-29 23:30:05 +0100

    document match service

--- src/match/match.service.ts
+++ src/match/match.service.ts
@@ -12,6 +12,13 @@ export class MatchService {
     testMap.map((pair) => [pair.bookingTestId, pair.claimTestId]),
   );
 
+  /**
+   * Matches bookings and claims lists, returning an array of matches with mismatched fields.
+   *
+   * @param bookings - Array of Booking objects to be matched.
+   * @param claims - Array of Claim objects to be matched.
+   * @returns Array of Match objects containing matched booking and claim IDs along with mismatched fields.
+   */
   matchLists(bookings: Booking[], claims: Claim[]): Match[] {
     if (!bookings.length || !claims.length) {
       return [];
@@ -26,6 +33,7 @@ export class MatchService {
     let claimsInd = 0;
     const matchesWithMismatchedFields: Match[] = [];
 
+    // Iterate through both grouped lists to find matches by patient
     do {
       const [bookingPatient, bookings] = groupedBookingsByPatient[bookingsInd];
       const [claimPatient, claims] = groupedClaimsByPatient[claimsInd];
@@ -48,6 +56,13 @@ export class MatchService {
     return matchesWithMismatchedFields;
   }
 
+  /**
+   * Sorts documents by a specified field.
+   *
+   * @param documents
+   * @param field
+   * @returns
+   */
   #sortDocuments<T>(documents: T[], field: string) {
     return documents.sort((docA, docB) => (docA[field] > docB[field] ? 1 : -1));
   }
@@ -58,11 +73,21 @@ export class MatchService {
     return date1.substring(0, 10) === date2.substring(0, 10);
   }
 
+  /**
+   * Matches documents from bookings and claims based on the the same date.
+   * assumes both lists are sorted by date.
+   *
+   * @param bookings Array of Booking objects.
+   * @param claims Array of Claim objects.
+   * @returns
+   */
   #matchDocuments(bookings: Booking[], claims: Claim[]): Match[] {
     let indB = 0;
     let indC = 0;
     const matchedDocuments: Match[] = [] as Match[];
     let mismatch: string[];
+
+    // As the lists are sorted come through both arrays in one pass with two indices
     do {
       if (
         this.#isTheSameDay(
@@ -88,6 +113,13 @@ export class MatchService {
     return matchedDocuments;
   }
 
+  /**
+   * Determines the mismatched fields between a booking and a claim.
+   *
+   * @param booking
+   * @param claim
+   * @returns Array of mismatched field names.
+   */
   #getMismatch(booking: Booking, claim: Claim): string[] {
     const mismatch: string[] = [];
     const claimTestId = this.#testMap.get(booking.test);
@@ -102,15 +134,20 @@ export class MatchService {
     return mismatch;
   }
 
+  /**
+   * Groups and sorts a list of documents by patient.
+   *
+   * @param list List of documents - Bookings or Claims
+   * @returns Grouped documents by Patient
+   */
   #groupAndSort<T extends { patient: string }>(list: T[]): Groped<T>[] {
     const groupedByPatient: Partial<Record<string, T[]>> = Object.groupBy(
       list,
       ({ patient }) => patient.trim(),
     );
 
-    return Object.entries(groupedByPatient)
-      .filter(([, items]) => items !== undefined)
-      .map(([key, items]) => [key, items!] as Groped<T>)
-      .sort(([patientA], [patientB]) => (patientA > patientB ? 1 : -1));
+    return (Object.entries(groupedByPatient) as Groped<T>[]).sort(
+      ([patientA], [patientB]) => (patientA > patientB ? 1 : -1),
+    );
   }
 }
